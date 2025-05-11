7e294932684689274ff410b4a9491109f9916141
Derradji Senani <derradji.senani.1@gmail.com>
2025-05-11 10:39:49 +0100

    Update ./Active-Directory/Resources-Monitor/main.ps1 - Add and Check Online Computers in Every Loop

--- Active-Directory/Resources-Monitor/main.ps1
+++ Active-Directory/Resources-Monitor/main.ps1
@@ -1,25 +1,30 @@
 $EventsPath = "$PSScriptRoot\Events.log"
+$ErrorsPath = "$PSScriptRoot\Errors.log"
 $csvPath = "$PSScriptRoot\MonitoringResult.csv"
 
-$ComputersOnlineDict = @{}
-$ComputersOfflineDict = @{}
-$Computers = @(
-    @{ Name = "DC12" }
-)
-
-foreach ($Computer in $Computers) {
-    try {
-        $ping = Test-Connection $Computer.Name -Count 1 -ErrorAction Stop
-        $ComputersOnlineDict[$Computer.Name] = "$($ping.IPV4Address.IPAddressToString) ON"
-        Add-Content -Value "$($Computer.Name),ON,$(Get-Date)" -Path $EventsPath
-    } catch {
-        $ComputersOfflineDict[$Computer.Name] = "$($Computer.Name) OFF"
-        Add-Content -Value "$($Computer.Name),OFF,$(Get-Date)" -Path $EventsPath
+function CheckOnlineDevices {
+    $ComputersOnlineDict = @{}
+    $ComputersOfflineDict = @{}
+    $Computers = Get-ADComputer -Filter * -Property Name
+    foreach ($Computer in $Computers) {
+        try {
+            $ping = Test-Connection $Computer.Name -Count 1 -ErrorAction Stop
+            $ComputersOnlineDict[$Computer.Name] = "$($ping.IPV4Address.IPAddressToString) ON"
+            Add-Content -Value "$($Computer.Name),ON,$(Get-Date)" -Path $EventsPath
+        } catch {
+            $ComputersOfflineDict[$Computer.Name] = "$($Computer.Name) OFF"
+            Add-Content -Value "$($Computer.Name),OFF,$(Get-Date)" -Path $EventsPath
+        }
     }
+
+    return $ComputersOnlineDict
 }
 
 while ($true) {
-    foreach ($ComputerName in $ComputersOnlineDict.Keys) {
+   $ComputersOnlineDict = CheckOnlineDevices
+   if ($null -ne $ComputersOnlineDict){
+     foreach ($ComputerName in $ComputersOnlineDict.Keys) {
+        #write-host $ComputerName
         try {
             $CPU = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
                 (Get-Counter '\Processor(_Total)\% Processor Time' -SampleInterval 1 -MaxSamples 5).CounterSamples |
@@ -43,28 +48,30 @@ while ($true) {
             $RAM_Value = [math]::Round(($RAM | Measure-Object -Average).Average)
 
             if (-not (Test-Path $csvPath)) {
-                Add-Content -Path $csvPath -Value "RamUsage,CpuUsage,DisksUsage,Alert,DateAndTime"
+                Add-Content -Path $csvPath -Value "Computer, H-Value,H-Alert,H-Processes,Time"
             }
 
             $DisksUs = $Disks
             $timestamp = Get-Date -Format 'MM-dd HH:mm:ss'
 
             # CPU Check
-            if ($CPU_Value -gt 10) {
+            if ($CPU_Value -gt 50) {
+                add-Content -Path $EventsPath -Value "CPU Alert : $CPU_Value in $timestamp"
                 $First3CpuProcesses = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
                     Get-Process | Sort-Object CPU -Descending | Select-Object -First 3 Name, @{Name="CPU_Sec"; Expression={[math]::Round($_.CPU, 2)}}
                 }
-                $msg = "$CPU_Value%,CPU alert,$($First3CpuProcesses.Name),$timestamp"
+                $msg = "$ComputerName,$CPU_Value%,CPU alert,$($First3CpuProcesses.Name) : $($First3CpuProcesses.CPU_Sec),$timestamp"
                 Write-Host $msg
                 Add-Content -Path $csvPath -Value $msg
             }
 
             # RAM Check
-            if ($RAM_Value -gt 40) {
+            if ($RAM_Value -gt 70) {
+                add-Content -Path $EventsPath -Value "RAM Alert : $RAM_Value in $timestamp"
                 $First3RamProcesses = Invoke-Command -ComputerName $ComputerName -ScriptBlock {
                     Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 3 Name, @{Name="RAM_MB"; Expression={[math]::Round($_.WorkingSet / 1MB, 2)}}
                 }
-                $msg = "$RAM_Value%,RAM alert,$($First3RamProcesses.Name),$timestamp"
+                $msg = "$ComputerName,$RAM_Value%,RAM alert,$($First3RamProcesses.Name) : $($First3RamProcesses.RAM_MB),$timestamp"
                 Write-Host $msg
                 Add-Content -Path $csvPath -Value $msg
             }
@@ -73,16 +80,24 @@ while ($true) {
             foreach ($data in $DisksUs.GetEnumerator()) {
                 $key = $data.Key
                 $Value = $data.Value
-
-                if ($Value -lt 5) {
-                    $msg = "Driver $key : Free $Value,Disk Alert,$timestamp"
+                if($value -ne 0){
+                if ($Value -lt 10) {
+                    add-Content -Path $EventsPath -Value "Disk Alert : Driver ($key) - Free Space ($Value) in $timestamp"
+                    $msg = "$ComputerName,Driver $key : Free $Value,Disk Alert,$timestamp"
                     Write-Host $msg
                     Add-Content -Path $csvPath -Value $msg
-                }
+                }}
             }
 
         } catch {
-            Write-Host "Error collecting data: $($_.Exception.Message)"
+            write-host "error to connect"
+            Add-Content -Path $ErrorsPath -Value "Error collecting data: $($_.Exception.Message) in $timestamp"
         }
     }
+    Start-Sleep -Seconds 300
+   }else {
+        $msg =  "There is no Online Computers"
+        Add-Content -Path $ErrorsPath -Value $msg : $timestamp
+        # Write-Host $msg
+   }
 }
