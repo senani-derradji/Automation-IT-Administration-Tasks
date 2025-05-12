9c7a48e0284e26e2c14a2e6d36fda44ecada24b3
Derradji Senani <derradji.senani.1@gmail.com>
2025-05-12 11:11:48 +0100

    Create \Resources-Monitor\SchedulerTask-Auto.ps1 ta make Scheduler Task work Automatically

--- Active-Directory/Resources-Monitor/SchedulerTask-Auto.ps1
+++ Active-Directory/Resources-Monitor/SchedulerTask-Auto.ps1
@@ -3,14 +3,14 @@ $scriptPath = "$PSScriptRoot\main.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
 
 $start = (Get-Date).AddSeconds(30)
-$interval = New-TimeSpan -Minutes 1
+$interval = New-TimeSpan -Minutes 5
 $end = [TimeSpan]::MaxValue
 
 $trigger = New-ScheduledTaskTrigger -Once -At $start -RepetitionInterval $interval -RepetitionDuration $end
 
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-# Start the Scheduler Task after 5 Seconds and Every 30 minuts ForEver
+# Start the Scheduler Task after 30 Seconds and Every 5 minuts ForEver
 Register-ScheduledTask -TaskName "MonitoringDC" -Action $action -Trigger $trigger -Principal $principal -Description "Derradji Monitoring Resources !!"
 
 # To Remove Scheduler Task 
